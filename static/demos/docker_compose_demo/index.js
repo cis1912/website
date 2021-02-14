712335501041670f3df012f76f6bf741656f7e2b
cphalen <cphalen@seas.upenn.edu>
2021-02-14 13:39:45 -0500

    Updates

--- static/demos/docker_compose_demo/index.js
+++ static/demos/docker_compose_demo/index.js
@@ -5,7 +5,7 @@ const hostname = '0.0.0.0';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  fs.appendFile('/etc/log/log.txt', JSON.stringify(req.headers) + "\n\n", (error, data) => {
+  fs.appendFile('/log/log.txt', JSON.stringify(req.headers) + "\n\n", (error, data) => {
     if (error) {
       return console.log(error);
     }
